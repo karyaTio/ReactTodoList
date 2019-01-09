03d7f456b95e47407fc5958e103414cbf7a9e8d2
Tio Saputra <karya.tiosaputra@gmail.com>
2019-01-09 19:47:54 +0700

    Checklist and delete todo

--- src/App.js
+++ src/App.js
@@ -24,11 +24,33 @@ class App extends Component {
     ]
   };
 
+  // Toggle task
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // Delete task
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+
   render() {
-    console.log(this.state.todos);
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 import TodoItem from "./TodoItem";
 
 class Todos extends Component {
   render() {
-    return this.props.todos.map(todo => <TodoItem key={todo.id} todo={todo} />);
+    return this.props.todos.map(todo => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+        delTodo={this.props.delTodo}
+      />
+    ));
   }
 }
+
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+};
 
 export default Todos;
